fad4d04ba028c63b9684e6969ddcc99d472c8a6b
AhmedAliMSoliman <ahmed-3010@hotmail.com>
2021-05-29 15:33:34 +0400

    First version of color game finsh

--- colorgame.js
+++ colorgame.js
@@ -1,6 +1,7 @@
 
-//List of clolr
-var colors = generateRandoColors(6);
+
+var numberOfSequare = 6;
+var colors = generateRandoColors(numberOfSequare);
 
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
@@ -8,12 +9,53 @@ var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
 var h1 = document.querySelector("h1");
 var resetButton = document.querySelector("#reset");
+var easyBtn = document.querySelector("#easyBtn");
+var hardBtn = document.querySelector("#hardBtn");
 
+easyBtn.addEventListener("click", function()
+{
+    hardBtn.classList.remove("selected");
+    easyBtn.classList.add("selected");
+
+    numberOfSequare = 3;
+    colors = generateRandoColors(numberOfSequare);
+    pickedColor = pickColor();
+    colorDisplay.textContent = pickedColor;
+
+    for(var i = 0 ; i < squares.length ; i++)
+    {
+        if(colors[i])
+        {
+            squares[i].style.backgroundColor = colors[i];
+        }
+        else
+        {
+            squares[i].style.display = "none";
+        }
+    }
+});
+
+hardBtn.addEventListener("click", function()
+{
+    hardBtn.classList.add("selected");
+    easyBtn.classList.remove("selected");
+
+    numberOfSequare = 6;
+    colors = generateRandoColors(numberOfSequare);
+    pickedColor = pickColor();
+    colorDisplay.textContent = pickedColor;
+
+    for(var i = 0 ; i < squares.length ; i++)
+    {
+        squares[i].style.backgroundColor = colors[i];
+        squares[i].style.display = "block";
+    }
+});
 
 resetButton.addEventListener("click", function()
 {
     //generate akk bew colors
-    colors = generateRandoColors(6);
+    colors = generateRandoColors(numberOfSequare);
 
     //pick a new random color from array
     pickedColor = pickColor();
@@ -26,7 +68,7 @@ resetButton.addEventListener("click", function()
     {
         squares[i].style.backgroundColor = colors[i];
     }
-    h1.style.backgroundColor = "#232323";
+    h1.style.backgroundColor = "#steelblue";
     messageDisplay.style.color = "white";
 });
 
