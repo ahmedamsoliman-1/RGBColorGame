eb9e7754e27531f8aee305a96834ea0c2b8e4b2d
AhmedAliMSoliman <ahmed-3010@hotmail.com>
2021-05-29 14:13:46 +0400

    Add the new color and try again features

--- colorgame.js
+++ colorgame.js
@@ -7,6 +7,28 @@ var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
 var h1 = document.querySelector("h1");
+var resetButton = document.querySelector("#reset");
+
+
+resetButton.addEventListener("click", function()
+{
+    //generate akk bew colors
+    colors = generateRandoColors(6);
+
+    //pick a new random color from array
+    pickedColor = pickColor();
+
+    //change colorDisplay to match picked color
+    colorDisplay.textContent = pickedColor;
+
+    //change colors of the squares
+    for (var i = 0 ; i < squares.length; i++)
+    {
+        squares[i].style.backgroundColor = colors[i];
+    }
+    h1.style.backgroundColor = "#232323";
+    messageDisplay.style.color = "white";
+});
 
 colorDisplay.textContent = pickedColor;
 
@@ -23,21 +45,23 @@ for (var i = 0 ; i < squares.length; i++)
         var clickedColor = this.style.backgroundColor;
 
         //compare color to picked color
-        console.log(clickedColor , pickedColor);
         if(clickedColor === pickedColor)
         {
             messageDisplay.textContent = "Correct";
+            resetButton.textContent = "Play Again!"
             changeColors(pickedColor);
             h1.style.backgroundColor = pickedColor;
         }
         else
         {
+            //gray out the wrong clicked one
             this.style.backgroundColor = "#232323";
             messageDisplay.textContent = "Try again!";
         }
     });
 }
 
+// Change all square colors after correct guess
 function changeColors(color)
 {
     //Loop through all squares
@@ -48,6 +72,7 @@ function changeColors(color)
     }
 }
 
+//Pick random number and map it to colors arr
 function pickColor()
 {
     var randomN = Math.floor(Math.random() * colors.length);
